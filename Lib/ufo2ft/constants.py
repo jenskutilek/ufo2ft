3aaf7866f63cc7e9749c9cc84a43cad5c7eb1c0f
Jens Kutilek <webmail@kutilek.de>
2021-12-26 10:48:36 +0100

    Re-add lost constants

--- Lib/ufo2ft/constants.py
+++ Lib/ufo2ft/constants.py
@@ -38,6 +38,12 @@ OPENTYPE_META_KEY = "public.openTypeMeta"
 
 UNICODE_VARIATION_SEQUENCES_KEY = "public.unicodeVariationSequences"
 
+TRUETYPE_INSTRUCTIONS_KEY = "public.truetype.instructions"
+TRUETYPE_ROUND_KEY = "public.truetype.roundOffsetToGrid"
+TRUETYPE_METRICS_KEY = "public.truetype.useMyMetrics"
+TRUETYPE_OVERLAP_KEY = "public.truetype.overlap"
+OBJECT_LIBS_KEY = "public.objectLibs"
+
 INDIC_SCRIPTS = [
     "Beng",  # Bengali
     "Cham",  # Cham
